7e08f1f1ec7a66b5ee0b1e9e9dca7e575842f847
gabsrodriguez <gmederos95@gmail.com>
2019-12-04 15:31:20 +0000

    update

--- public/bundle.js
+++ public/bundle.js
@@ -720,8 +720,8 @@ function render(variables = {}) {
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/" ${variables.twitter}><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/" ${variables.github}><i class="fa fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/"${variables.linkedin}><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/" ${variables.linkedin}><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/"${variables.instagram}><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
